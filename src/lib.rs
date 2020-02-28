420bb00cce5011e9ef7214dfb458e4f6cbb3b561
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-02-28 03:14:54 +0100

    uri: fix bugs + clean code

--- src/lib.rs
+++ src/lib.rs
@@ -10,8 +10,12 @@ pub fn squeeze_uri(s: &str) -> Option<&str> {
         return None;
     }
     idx += advance_hier_part(&input[idx..])?;
-    idx += advance_query(&input[idx..]).unwrap_or(0);
-    idx += advance_fragment(&input[idx..]).unwrap_or(0);
+    idx += advance_question_mark(&input[idx..])
+        .map(|j| j + advance_query(&input[idx + j..]))
+        .unwrap_or(0);
+    idx += advance_sharp(&input[idx..])
+        .map(|j| j + advance_fragment(&input[idx + j..]))
+        .unwrap_or(0);
 
     Some(&s[scheme_idx..idx])
 }
@@ -31,6 +35,10 @@ fn find_scheme(input: &[u8]) -> Option<usize> {
     scheme_idx
 }
 
+// hier-part = "//" authority path-abempty
+//           / path-absolute
+//           / path-rootless
+//           / path-empty
 fn advance_hier_part(input: &[u8]) -> Option<usize> {
     // "//" authority path-abempty
     if let Some(idx) = advance_slash_slash(input)
@@ -60,30 +68,14 @@ fn advance_hier_part(input: &[u8]) -> Option<usize> {
     Some(0)
 }
 
-// "/"
-fn advance_slash(input: &[u8]) -> Option<usize> {
-    if input.len() >= 1 && input[0] == b'/' {
-        Some(1)
-    } else {
-        None
-    }
-}
-
-// "//"
-fn advance_slash_slash(input: &[u8]) -> Option<usize> {
-    if input.len() >= 2 && input[0] == b'/' && input[1] == b'/' {
-        Some(2)
-    } else {
-        None
-    }
-}
-
 // [ userinfo "@" ] host [ ":" port ]
 fn advance_authority(input: &[u8]) -> Option<usize> {
     let mut idx = 0;
-    idx += advance_user_info(&input[idx..]).unwrap_or(0);
+    idx += advance_userinfo(&input[idx..]).unwrap_or(0);
     idx += advance_hostname(&input[idx..])?;
-    idx += advance_port(&input[idx..]).unwrap_or(0);
+    idx += advance_colon(&input[idx..])
+        .map(|j| j + advance_port(&input[idx + j..]))
+        .unwrap_or(0);
     Some(idx)
 }
 
@@ -119,9 +111,9 @@ fn advance_segment_nz(input: &[u8]) -> Option<usize> {
     }
 }
 
-fn advance_user_info(input: &[u8]) -> Option<usize> {
+fn advance_userinfo(input: &[u8]) -> Option<usize> {
     let arobase_idx = input.iter().position(|&b| b == b'@')?;
-    if is_user_info(&input[..arobase_idx]) {
+    if is_userinfo(&input[..arobase_idx]) {
         Some(arobase_idx + 1)
     } else {
         None
@@ -137,80 +129,108 @@ fn advance_hostname(input: &[u8]) -> Option<usize> {
     }
 }
 
-fn advance_port(input: &[u8]) -> Option<usize> {
-    let mut idx = 0;
-    if input[idx] != b':' {
-        return None;
-    }
-    idx += 1;
-    idx += &input[idx..].iter().take_while(|&&c| is_digit(c)).count();
-    Some(idx)
+// *DIGIT
+fn advance_port(input: &[u8]) -> usize {
+    input.iter().take_while(|&&c| is_digit(c)).count()
 }
 
-// https://tools.ietf.org/html/rfc3986#section-3.4
-fn advance_query(input: &[u8]) -> Option<usize> {
+// *( pchar / "/" / "?" )
+fn advance_query(input: &[u8]) -> usize {
     let mut idx = 0;
-    if input[idx] != b'?' {
-        return None;
-    }
-    idx += 1;
     while idx < input.len() {
         if let Some(i) = advance_pchar(&input[idx..]) {
             idx += i;
             continue;
         }
-        let c = input[idx];
-        if c == b'/' || c == b'?' {
+        if [b'/', b'?'].contains(&input[idx]) {
             idx += 1;
             continue;
         }
         break;
     }
-    Some(idx)
+    idx
 }
 
-// https://tools.ietf.org/html/rfc3986#section-3.5
-fn advance_fragment(input: &[u8]) -> Option<usize> {
+// *( pchar / "/" / "?" )
+fn advance_fragment(input: &[u8]) -> usize {
     let mut idx = 0;
-    if input[idx] != b'#' {
-        return None;
-    }
-    idx += 1;
     while idx < input.len() {
         if let Some(i) = advance_pchar(&input[idx..]) {
             idx += i;
             continue;
         }
-        let c = input[idx];
-        if c == b'/' || c == b'?' {
+        if [b'/', b'?'].contains(&input[idx]) {
             idx += 1;
             continue;
         }
         break;
     }
-    Some(idx)
+    idx
 }
 
+// unreserved / pct-encoded / sub-delims / ":" / "@"
 fn advance_pchar(input: &[u8]) -> Option<usize> {
     if let Some(idx) = advance_pct_encoded(input) {
         return Some(idx);
     }
     let c = input[0];
-    if is_unreserved(c) || is_sub_delim(c) || c == b':' || c == b'@' {
+    if is_unreserved(c) || is_sub_delim(c) || [b':', b'@'].contains(&c) {
         return Some(1);
     }
     None
 }
 
+// "%" HEXDIG HEXDIG
 fn advance_pct_encoded(input: &[u8]) -> Option<usize> {
-    if input.len() >= 3 && input[0] == b'%' && is_hexa(input[1]) && is_hexa(input[2]) {
+    if input.len() >= 3 && input[0] == b'%' && is_hexdig(input[1]) && is_hexdig(input[2]) {
         Some(3)
     } else {
         None
     }
 }
 
-fn is_user_info(input: &[u8]) -> bool {
+fn advance_colon(input: &[u8]) -> Option<usize> {
+    if input.len() >= 1 && input[0] == b':' {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+fn advance_question_mark(input: &[u8]) -> Option<usize> {
+    if input.len() >= 1 && input[0] == b'?' {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+fn advance_sharp(input: &[u8]) -> Option<usize> {
+    if input.len() >= 1 && input[0] == b'#' {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+fn advance_slash(input: &[u8]) -> Option<usize> {
+    if input.len() > 0 && input[0] == b'/' {
+        Some(1)
+    } else {
+        None
+    }
+}
+
+fn advance_slash_slash(input: &[u8]) -> Option<usize> {
+    if input.len() > 1 && input[0] == b'/' && input[1] == b'/' {
+        Some(2)
+    } else {
+        None
+    }
+}
+
+// *( unreserved / pct-encoded / sub-delims / ":" )
+fn is_userinfo(input: &[u8]) -> bool {
     let mut idx = 0;
     while idx < input.len() {
         if let Some(i) = advance_pct_encoded(&input[idx..]) {
@@ -227,22 +247,12 @@ fn is_user_info(input: &[u8]) -> bool {
     true
 }
 
-fn is_segment(input: &[u8]) -> bool {
-    let mut idx = 0;
-    while idx < input.len() {
-        if let Some(i) = advance_pchar(&input[idx..]) {
-            idx += i;
-            continue;
-        }
-        return false;
-    }
-    true
-}
-
+// ALPHA / DIGIT / "-" / "." / "_" / "~"
 fn is_unreserved(c: u8) -> bool {
     is_alpha(c) || is_digit(c) || c == b'-' || c == b'.' || c == b'_' || c == b'~'
 }
 
+// "!" / "$" / "&" / "'" / "(" / ")" / "*" / "+" / "," / ";" / "="
 fn is_sub_delim(c: u8) -> bool {
     [
         b'!', b'$', b'&', b'\'', b'(', b')', b'*', b'+', b',', b';', b'=',
@@ -250,14 +260,37 @@ fn is_sub_delim(c: u8) -> bool {
     .contains(&c)
 }
 
+// ALPHA
 fn is_alpha(c: u8) -> bool {
     (c >= b'a' && c <= b'z') || (c >= b'A' && c <= b'Z')
 }
 
-fn is_hexa(c: u8) -> bool {
+// CR
+fn is_cr(c: u8) -> bool {
+    c == b'\r'
+}
+
+// DIGIT
+fn is_digit(c: u8) -> bool {
+    c >= b'0' && c <= b'9'
+}
+
+// DQUOTE
+fn is_dquote(c: u8) -> bool {
+    c == b'"'
+}
+
+// HEXDIG
+fn is_hexdig(c: u8) -> bool {
     is_digit(c) && (c >= b'a' && c <= b'f' || c >= b'A' && c <= b'F')
 }
 
-fn is_digit(c: u8) -> bool {
-    c >= b'0' && c <= b'9'
+// LF
+fn is_lf(c: u8) -> bool {
+    c == b'\n'
+}
+
+// SP
+fn is_sp(c: u8) -> bool {
+    c == b' '
 }
--- tests/uri.rs
+++ tests/uri.rs
@@ -2,69 +2,93 @@ use squeeze;
 
 #[test]
 fn it_should_extract_valid_uris() {
-    assert_eq!(
-        Some("http://localhost"),
-        squeeze::squeeze_uri("-> http://localhost <-")
-    );
-    // userinfo
-    assert_eq!(
-        Some("http://foobar:@localhost"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost <-")
-    );
-    assert_eq!(
-        Some("http://foobar:baz@localhost"),
-        squeeze::squeeze_uri("-> http://foobar:baz@localhost <-")
-    );
-    // port
-    assert_eq!(
-        Some("http://foobar:@localhost:"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost: <-")
-    );
-    assert_eq!(
-        Some("http://foobar:@localhost:8080"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080 <-")
-    );
-    // path
-    assert_eq!(
-        Some("http://localhost/lorem"),
-        squeeze::squeeze_uri("-> http://localhost/lorem <-")
-    );
-    // query
-    assert_eq!(
-        Some("http://foobar:@localhost:8080?"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080? <-")
-    );
-    assert_eq!(
-        Some("http://foobar:@localhost:8080?a=b"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080?a=b <-")
-    );
-    // fragment
-    assert_eq!(
-        Some("http://foobar:@localhost:8080#"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080# <-")
-    );
-    assert_eq!(
-        Some("http://foobar:@localhost:8080?#"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080?# <-")
-    );
-    assert_eq!(
-        Some("http://foobar:@localhost:8080?a=b#"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080?a=b# <-")
-    );
-    assert_eq!(
-        Some("http://foobar:@localhost:8080?a=b#c=d"),
-        squeeze::squeeze_uri("-> http://foobar:@localhost:8080?a=b#c=d <-")
-    );
-    // mailto
-    assert_eq!(
-        Some("mailto:fred@example.com"),
-        squeeze::squeeze_uri("-> <mailto:fred@example.com> <-")
-    );
+    for (output, input) in vec![
+        // basic
+        ("http://localhost", "-> http://localhost <-"),
+        // userinfo
+        ("http://foobar:@localhost", "-> http://foobar:@localhost <-"),
+        (
+            "http://foobar:baz@localhost",
+            "-> http://foobar:baz@localhost <-",
+        ),
+        // port
+        (
+            "http://foobar:@localhost:",
+            "-> http://foobar:@localhost: <-",
+        ),
+        (
+            "http://foobar:@localhost:8080",
+            "-> http://foobar:@localhost:8080 <-",
+        ),
+        // path
+        ("http://localhost/lorem", "-> http://localhost/lorem <-"),
+        // query
+        (
+            "http://foobar:@localhost:8080?",
+            "-> http://foobar:@localhost:8080? <-",
+        ),
+        (
+            "http://foobar:@localhost:8080?a=b",
+            "-> http://foobar:@localhost:8080?a=b <-",
+        ),
+        // fragment
+        (
+            "http://foobar:@localhost:8080#",
+            "-> http://foobar:@localhost:8080# <-",
+        ),
+        (
+            "http://foobar:@localhost:8080?#",
+            "-> http://foobar:@localhost:8080?# <-",
+        ),
+        (
+            "http://foobar:@localhost:8080?a=b#",
+            "-> http://foobar:@localhost:8080?a=b# <-",
+        ),
+        (
+            "http://foobar:@localhost:8080?a=b#c=d",
+            "-> http://foobar:@localhost:8080?a=b#c=d <-",
+        ),
+        // meh
+        ("http://:@localhost:/?#", "http://:@localhost:/?#"),
+        // various examples from the rfc
+        //("file:///etc/hosts", "file:///etc/hosts"),
+        ("http://localhost/", "http://localhost/"),
+        ("mailto:fred@example.com", "<mailto:fred@example.com>"),
+        //(
+        //"foo://info.example.com?fred",
+        //"<foo://info.example.com?fred>",
+        //),
+        //(
+        //"ftp://ftp.is.co.za/rfc/rfc1808.txt",
+        //"ftp://ftp.is.co.za/rfc/rfc1808.txt",
+        //),
+        //(
+        //"http://www.ietf.org/rfc/rfc2396.txt",
+        //"http://www.ietf.org/rfc/rfc2396.txt",
+        //),
+        //(
+        //"ldap://[2001:db8::7]/c=GB?objectClass?one",
+        //"ldap://[2001:db8::7]/c=GB?objectClass?one",
+        //),
+        ("mailto:John.Doe@example.com", "mailto:John.Doe@example.com"),
+        (
+            "news:comp.infosystems.www.servers.unix",
+            "news:comp.infosystems.www.servers.unix",
+        ),
+        ("tel:+1-816-555-1212", "tel:+1-816-555-1212"),
+        //("telnet://192.0.2.16:80/", "telnet://192.0.2.16:80/"),
+        (
+            "urn:oasis:names:specification:docbook:dtd:xml:4.1.2",
+            "urn:oasis:names:specification:docbook:dtd:xml:4.1.2",
+        ),
+    ] {
+        assert_eq!(Some(output), squeeze::squeeze_uri(input));
+    }
 }
 
 #[test]
 fn it_should_not_extract_invalid_uris() {
-    assert_eq!(None, squeeze::squeeze_uri(""));
-    assert_eq!(None, squeeze::squeeze_uri(" "));
-    assert_eq!(None, squeeze::squeeze_uri(":"));
+    for input in vec!["", " ", ":", ":/", "::", "-:"] {
+        assert_eq!(None, squeeze::squeeze_uri(input));
+    }
 }
