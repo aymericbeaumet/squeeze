cdfa4f19294766ca173dcf2e59fd152beab6944f
Aymeric Beaumet <hi@aymericbeaumet.com>
2020-03-03 20:17:49 +0100

    improve cli flags + add logger

--- src/squeeze-cli/main.rs
+++ src/squeeze-cli/main.rs
@@ -1,25 +1,34 @@
 use clap::Clap;
+use log::debug;
 use std::io::{self, BufRead};
 
 #[derive(Clap)]
-#[clap(version = "1.0", author = "Aymeric Beaumet <hi@aymericbeaumet.com>")]
+#[clap(
+    name = "squeeze",
+    version = "1.0",
+    author = "Aymeric Beaumet <hi@aymericbeaumet.com>"
+)]
 struct Opts {
-    #[clap(short = "1")]
+    #[clap(short = "1", long = "--first", help = "only keep the first result")]
     one: bool,
-    #[clap(long = "uri")]
+    #[clap(long = "uri", help = "attempt to match absolute uris")]
     uri: bool,
 }
 
 fn main() {
+    pretty_env_logger::init();
     let opts = Opts::parse();
 
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         let line = &line.unwrap();
-        if let (true, Some(uri)) = (opts.uri, squeeze::uri::find(line)) {
-            println!("{}", uri);
-            if opts.one {
-                break;
+        debug!("INPUT LINE \"{}\"", line);
+        if opts.uri {
+            if let Some(uri) = squeeze::uri::find(line) {
+                println!("{}", uri);
+                if opts.one {
+                    break;
+                }
             }
         }
     }
